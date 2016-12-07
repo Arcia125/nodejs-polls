a7680ef0eac0ac5018c748ca7f3e0ab70789988b
Arcia125 <tauren125@gmail.com>
2016-12-07 16:43:00 -0600

    add twitter Oauth with passport

--- app.js
+++ app.js
@@ -2,12 +2,79 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const cookieParser = require('cookie-parser');
+const serveStatic = require('serve-static');
+const bodyParser = require('body-parser');
+const expressSession = require('express-session');
+
+const passport = require('passport');
+const localStrategy = require('passport-local').Strategy;
+const TwitterStrategy = require('passport-twitter').Strategy;
 
 const config = require('./config');
 const db = require('./db');
 
+
+
+passport.use(new localStrategy(
+    function(username, password, done) {
+        User.findOne({ username: username }, function (err, user) {
+            if (err) {
+                return done(err);
+            }
+
+            if (!user) {
+                return done(null, false, { message: 'Incorrect username.' });
+            }
+
+            if (!user.validPassword(password)) {
+                return done(null, false, { message: 'Incorrect password'})
+            }
+
+            return done(null, user);
+        });
+    }
+));
+
+passport.use(new TwitterStrategy({
+    consumerKey: config.twitter.c_key,
+    consumerSecret: config.twitter.c_secret,
+    callbackURL: `${config.hostName}/auth/twitter/callback`
+    },
+    function(token, tokenSecret, profile, done) {
+        // User.findOrCreate(..., function(err, user) {
+        //     if (err) {
+        //         return done(err);
+        //     }
+        //     done(null, user);
+        // });
+        console.log(token, tokenSecret, profile, done);
+    }
+));
+
+
+
+
+app.use(serveStatic('public'));
+app.use(cookieParser());
+app.use(bodyParser());
+app.use(expressSession({ secret: 'test1' }));
+app.use(passport.initialize());
+app.use(passport.session());
+// app.use(app.router);
+
+
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
+    res.sendFile(path.join(__dirname, '/views/index.html'));
+});
+
+app.get('/auth/twitter', passport.authenticate('twitter'));
+
+app.get('/auth/twitter/callback',
+    passport.authenticate('twitter', { successRedirect: '/', failureRedirect: '/login' }));
+
+app.get('/login', (req, res) => {
+    res.send(path.join(__dirname, '/views/index.html'));
 });
 
 app.get('/api/search/:search', (req, res) => {
@@ -17,6 +84,9 @@ app.get('/api/search/:search', (req, res) => {
 app.get('/api/history', (req, res) => {
     res.send('route2');
 });
+
+app.post('/login',
+    passport.authenticate('local', { successRedirect: '/', failureRedirect: '/login', failureFlash: true }));
 
 db.connect(config.db.url, function(err) {
     if (err) {
--- config.js
+++ config.js
@@ -2,6 +2,8 @@
 
 const config = {};
 config.port = process.env.PORT;
+config.isDev = !!process.env.DEV;
+config.hostName = config.isDev ? `localhost:${config.port}` : 'https://nodejs-polls.herokuapp.com';
 
 config.db = {};
 config.db.host = process.env.DBHOST;
@@ -10,5 +12,9 @@ config.db.pw = process.env.DBPW;
 config.db.port = process.env.DBPORT;
 config.db.name = process.env.DBNAME;
 config.db.url = `mongodb://${config.db.user}:${config.db.pw}@${config.db.host}:${config.db.port}/${config.db.name}`;
+
+config.twitter = {};
+config.twitter.c_key = process.env.TWITTER_CONSUMER_KEY;
+config.twitter.c_secret = process.env.TWITTER_CONSUMER_SECRET;
 
 module.exports = config;
