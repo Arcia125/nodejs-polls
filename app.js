08bdafabc156ac3bbac4ac707d1a3734e7bf6d22
Arcia125 <tauren125@gmail.com>
2016-12-07 18:10:13 -0600

    bug fixing

--- app.js
+++ app.js
@@ -73,10 +73,10 @@ passport.deserializeUser((id, done) => {
 
 
 
-app.use(serveStatic('public'));
-app.use(cookieParser());
-app.use(bodyParser());
-app.use(expressSession({ secret: 'test1' }));
+// app.use(serveStatic('public'));
+// app.use(cookieParser());
+// app.use(bodyParser());
+// app.use(expressSession({ secret: 'test1' }));
 app.use(passport.initialize());
 app.use(passport.session());
 
