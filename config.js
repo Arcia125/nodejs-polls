2bdcc95ea46327b19e5c76bdb2a1d6ed0bf40e7d
Kevin Hallett <kevinmhallett@gmail.com>
2019-01-21 09:35:28 -0600

    update config

--- config.js
+++ config.js
@@ -1,20 +1,35 @@
-'use strict';
+module.exports = (({
+  DEV,
+  DBHOST,
+  DBUSER,
+  DBPW,
+  DBPORT,
+  DBNAME,
+  PORT,
+  TWITTER_CONSUMER_KEY,
+  TWITTER_CONSUMER_SECRET
+}) => {
+  return {
+    port: PORT,
+    isDev: DEV,
+    hostName: DEV
+      ? `http://localhost:${PORT}`
+      : `https://nodejs-polls.herokuapp.com`,
+    db: {
+      host: DBHOST,
+      user: DBUSER,
+      pw: DBPW,
+      port: DBPORT,
+      name: DBNAME,
+      url: `mongodb://${DBUSER}:${DBPW}@${DBHOST}:${DBPORT}/${DBNAME}`
+    },
+    twitter: {
+      c_key: TWITTER_CONSUMER_KEY,
+      c_secret: TWITTER_CONSUMER_SECRET
+    }
+  };
+})(process.env);
 
-const config = {};
-config.port = process.env.PORT;
-config.isDev = !!process.env.DEV;
-config.hostName = config.isDev ? `http://localhost:${config.port}` : `https://nodejs-polls.herokuapp.com`;
-
-config.db = {};
-config.db.host = process.env.DBHOST;
-config.db.user = process.env.DBUSER;
-config.db.pw = process.env.DBPW;
-config.db.port = process.env.DBPORT;
-config.db.name = process.env.DBNAME;
-config.db.url = `mongodb://${config.db.user}:${config.db.pw}@${config.db.host}:${config.db.port}/${config.db.name}`;
-
-config.twitter = {};
-config.twitter.c_key = process.env.TWITTER_CONSUMER_KEY;
-config.twitter.c_secret = process.env.TWITTER_CONSUMER_SECRET;
-
-module.exports = config;
+if (require.main) {
+  console.log(module.exports);
+}
